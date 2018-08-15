a6b684dd883c50672dea353a36755414e3f94a93
ElRaptorus <christian.werner@5minds.de>
2018-08-15 13:11:56 +0200

    Import timing service implementation from process engine

--- src/timing_service.ts
+++ src/timing_service.ts
@@ -1,5 +1,5 @@
 import {IEventAggregator} from '@essential-projects/event_aggregator_contracts';
-import {ITimerEntity, ITimingRule, ITimerRepository, Timer, ITimingService, TimerType} from '@essential-projects/timing_contracts';
+import {ITimerRepository, ITimerService, TimerType, TimerRule, Timer} from '@essential-projects/timing_contracts';
 
 import * as moment from 'moment';
 import * as schedule from 'node-schedule';
@@ -8,7 +8,7 @@ interface IJobsCache {
   [timerId: string]: schedule.Job;
 }
 
-export class TimingService implements ITimingService {
+export class TimerService implements ITimerService {
 
   private _jobs: IJobsCache = {};
 
@@ -32,46 +32,138 @@ export class TimingService implements ITimingService {
     return this._timerRepository;
   }
 
+  public async initialize(): Promise<void> {
+    return this.restorePersistedJobs();
+  }
+
   public async cancel(timerId: string): Promise<void> {
 
-    const job = this._getJob(timerId);
+    const job: schedule.Job = this._getJob(timerId);
 
     if (job) {
-
       schedule.cancelJob(job);
-
       this._removeJob(timerId);
     }
 
-    return this._removeTimer(timerId);
+    await this.timerRepository.removeById(timerId);
   }
 
   public async once(date: moment.Moment, eventName: string): Promise<string> {
 
     if (!date) {
-      throw new Error('invalid date');
+      throw new Error('Must provide an expiration date for a one-shot timer!');
     }
 
     return this._createTimer(TimerType.once, date, undefined, eventName);
   }
 
-  public async periodic(rule: ITimingRule, eventName: string): Promise<string> {
+  public async periodic(rule: TimerRule, eventName: string): Promise<string> {
 
     if (!rule) {
-      throw new Error('invalid date');
+      throw new Error('Must provide a rule for a periodic timer!');
     }
 
     return this._createTimer(TimerType.periodic, undefined, rule, eventName);
   }
 
+  private async _createTimer(timerType: TimerType,
+                             timerDate: moment.Moment,
+                             timerRule: TimerRule,
+                             eventName: string): Promise<string> {
+
+    const timerData: any = {
+      type: timerType,
+      expirationDate: timerDate || null,
+      rule: timerRule,
+      eventName: eventName,
+    };
+
+    const timerIsValidTimerEntry: Boolean = this._isValidTimer(timerData);
+
+    const createdTimerId: string = await this.timerRepository.create(timerData);
+
+    if (timerIsValidTimerEntry) {
+      timerData.id = createdTimerId;
+      this._createJob(createdTimerId, timerData, eventName);
+    }
+
+    return createdTimerId;
+  }
+
+  public async restorePersistedJobs(): Promise<void> {
+
+    const persistedTimers: Array<Timer> = await this.timerRepository.getAll();
+
+    const filteredTimers: Array<Timer> = this._getValidTimersToRestoreFromList(persistedTimers);
+
+    for (const timer of filteredTimers) {
+      const timerIsValidTimerEntry: Boolean = this._isValidTimer(timer);
+
+      if (timerIsValidTimerEntry) {
+        this._createJob(timer.id, timer, timer.eventName);
+      }
+    }
+  }
+
+  private _getValidTimersToRestoreFromList(timers: Array<Timer>): Array<Timer> {
+
+    const filteredTimers: Array<Timer> = timers.filter((timer: Timer): boolean => {
+
+      const isNotOneShotTimer: boolean = timer.type !== TimerType.once;
+      const isValidOneShotTimer: boolean =
+        timer.type === TimerType.once &&
+        (timer.lastElapsed === undefined || timer.lastElapsed === null);
+
+      return isNotOneShotTimer || isValidOneShotTimer;
+    });
+
+    return filteredTimers;
+  }
+
+  private _isValidTimer(timer: Timer): boolean {
+
+    const timerIsOneShotTimer: boolean = timer.type === this.oneShotTimerType;
+
+    let isValidTimer: boolean = true;
+
+    if (timerIsOneShotTimer) {
+
+      if (!timer.expirationDate) {
+        return false;
+      }
+
+      const timerDate: moment.Moment = timer.expirationDate;
+      const now: moment.Moment = moment();
+
+      const exectionTimeIsBefore: boolean = timerDate.isAfter(now);
+
+      isValidTimer = timer.lastElapsed !== null || exectionTimeIsBefore;
+    }
+
+    return isValidTimer;
+  }
+
+  private _createJob(timerId: string, timer: Timer, eventName: string): schedule.Job {
+
+    const timerValue: TimerRule | Date = timer.type === TimerType.periodic
+        ? timer.rule
+        : timer.expirationDate.toDate();
+
+    const job: schedule.Job = schedule.scheduleJob(timerValue, async() => {
+      return this._timerElapsed(timerId, eventName);
+    });
+
+    if (!job) {
+      throw new Error('an error occured during job scheduling');
+    }
+
+    this._cacheJob(timerId, job);
+
+    return job;
+  }
+
   private async _timerElapsed(timerId: string, eventName: string): Promise<void> {
-
-    const timer = await this._getTimerById(timerId);
-
-    timer.lastElapsed = new Date();
-
-    await this.timerRepository.save(timer);
-
+    await this.timerRepository.setLastElapsedById(timerId, new Date());
     this.eventAggregator.publish(eventName);
   }
 
@@ -86,97 +178,6 @@ export class TimingService implements ITimingService {
   private _removeJob(timerId: string): void {
     if (this._jobs[timerId]) {
       delete this._jobs[timerId];
-    }
-  }
-
-  private async _getTimerById(timerId: string): Promise<Timer> {
-
-    const timer = await this._timerRepository.getById(timerId);
-
-    if (!timer) {
-      throw new Error(`an error occured during cancellation of job with id "${timerId}": not found`);
-    }
-
-    return timer;
-  }
-
-  private async _removeTimer(timerId: string): Promise<void> {
-
-    const timer = await this._getTimerById(timerId);
-
-    if (timer) {
-      await this.timerRepository.removeById(timer.id);
-    }
-  }
-
-  private async _createTimer(timerType: TimerType, timerDate: moment.Moment, timerRule: ITimingRule, eventName: string): Promise<string> {
-
-    const timer = new Timer();
-
-    timer.timerType = timerType;
-    timer.timerIsoString = timerDate ? timerDate.toISOString() : null;
-    timer.timerRule = timerRule;
-    timer.eventName = eventName;
-
-    const timerValue: ITimingRule | Date = timerType === TimerType.periodic ? timerRule : timerDate.toDate();
-
-    const timerIsValidTimerEntry: Boolean = this._isValidTimer(timer);
-
-    if (timerIsValidTimerEntry) {
-      this._createJob(timer.id, timerValue, eventName);
-    }
-
-    await this.timerRepository.save(timer);
-
-    return timer.id;
-  }
-
-  private _isValidTimer(timer: ITimerEntity): boolean {
-
-    const timerIsOneShotTimer: boolean = timer.timerType === this.oneShotTimerType;
-
-    let isValidTimer: boolean = true;
-
-    if (timerIsOneShotTimer) {
-
-      const timerDate: moment.Moment = moment(timer.timerIsoString);
-      const now: moment.Moment = moment();
-
-      const exectionTimeIsBefore: boolean = timerDate.isAfter(now);
-
-      isValidTimer = timer.lastElapsed !== null || exectionTimeIsBefore;
-    }
-
-    return isValidTimer;
-  }
-
-  private _createJob(timerId: string, timerValue: ITimingRule | string | Date, eventName: string): schedule.Job {
-
-    const job = schedule.scheduleJob(timerValue, async() => {
-      return this._timerElapsed(timerId, eventName);
-    });
-
-    if (!job) {
-      throw new Error('an error occured during job scheduling');
-    }
-
-    this._cacheJob(timerId, job);
-
-    return job;
-  }
-
-  public async restorePersistedJobs(): Promise<void> {
-
-    const timers = await this.timerRepository.all();
-
-    for (const timer of timers) {
-
-      const timerIsValidTimerEntry: Boolean = this._isValidTimer(timer);
-
-      if (timerIsValidTimerEntry) {
-        const timerValue: ITimingRule | string = timer.timerType === TimerType.periodic ? timer.timerRule : timer.timerIsoString;
-        this._createJob(timer.id, timerValue, timer.eventName);
-      }
     }
   }
 }
